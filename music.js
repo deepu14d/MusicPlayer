109322739036b68a6ac2c650aa180e7eb89f153d
deepu14d <deepanshu2002d@gmail.com>
2022-08-15 16:45:08 +0530

    separate music list created

---
+++ music.js
@@ -0,0 +1,20 @@
+const songs = [{
+    name: "song-1",
+    title: "I'm dangerous",
+    artist: "The EverLove"
+},
+{
+    name: "song-2",
+    title: "Scars",
+    artist: "Neffex"
+},
+{
+    name: "song-3",
+    title: "Tune jo na kaha",
+    artist: "Mohit Chauhan"
+},
+{
+    name: "song-4",
+    title: "On & On",
+    artist: "Cartoon"
+}]
